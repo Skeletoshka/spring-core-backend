8931760b0d1be9577cea2d327af6962f085376dd
Nikita <skeletonpro1@mail.ru>
2022-08-17 10:19:33 +0500

    доведение RowMapForEntity до ума

--- src/main/java/biz/bna/core/repository/TableRepository.java
+++ src/main/java/biz/bna/core/repository/TableRepository.java
@@ -8,24 +8,29 @@ import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.util.HashMap;
-import java.util.List;
-import java.util.Locale;
-import java.util.Map;
+import java.util.*;
 
 @Transactional(propagation = Propagation.REQUIRED)
 public interface TableRepository<T> {
-    //TODO Решить какие методы нужны для реализации
+    /**Класс содержит основные методы для доступа к бд. Возвращаются данные из ОДНОЙ таблицы.
+     * Методы: List<T> getAll()
+     * */
 
     //Заполняется при запуске приложения. См. config/InitApp
     Map<String, TableMetadata> metaDataMap = new HashMap<>();
 
     default List<T> getAll(){
         NamedParameterJdbcTemplate jdbc = OrmUtils.getJDBC();
-        Map<String, TableMetadata> metaDataMap = this.metaDataMap;
         String sql = "SELECT * FROM " + OrmUtils.getTableName(this.getClass());
         OrmUtils.loggerSql(sql);
         RowMapForEntity rowMapper = new RowMapForEntity(metaDataMap.get(OrmUtils.getTableName(this.getClass()).toLowerCase(Locale.ROOT)).getModelClass());
         return (List<T>) jdbc.query(sql, rowMapper);
+    }
+
+    default Integer count(){
+        NamedParameterJdbcTemplate jdbc = OrmUtils.getJDBC();
+        String sql = "SELECT COUNT(*) FROM " + OrmUtils.getTableName(this.getClass());
+        OrmUtils.loggerSql(sql);
+        return jdbc.queryForObject(sql, (Map<String, ?>) null, Integer.class);
     }
 }
