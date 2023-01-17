358853729f3ac61760fb78aa64ca821f316ebcbb
bna <skeletonpro1@mail.ru>
2023-01-17 14:41:10 +0500

    инициализация класса для грида + примерные поля

---
+++ src/main/java/biz/spring/core/utils/GridDataOption.java
@@ -0,0 +1,13 @@
+package biz.spring.core.utils;
+
+import java.util.Map;
+
+public class GridDataOption {
+    private String namedFilters;
+    private Map<String, Object> params;
+    private Integer rowCount;
+    private Integer page;
+    private String orderBy;
+    private String from;
+
+}
