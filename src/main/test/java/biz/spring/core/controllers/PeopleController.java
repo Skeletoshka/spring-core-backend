66955216bdf2614c106ebdfc4c0ba6255d2fdb33
mihajcy <m.morozov.perm@gmail.com>
2023-02-04 14:19:37 +0500

    Исправление ошибок, дополнение

---
+++ src/main/test/java/biz/spring/core/controllers/PeopleController.java
@@ -0,0 +1,48 @@
+package biz.spring.core.controllers;
+
+import biz.spring.core.payload.request.LoginRequest;
+import biz.spring.core.payload.response.JwtResponse;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.BeforeAll;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.annotation.Rollback;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.transaction.annotation.Transactional;
+
+import static org.hamcrest.Matchers.containsString;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+public class PeopleControllerTest extends IntegratedTest{
+    @Test
+    @Rollback
+    @Transactional
+    public void getListTest() throws Exception{
+        String id = "1";
+        this.mockMvc.perform(post("/api/people/getlist")
+                        .content("1")
+                        .contentType(MediaType.APPLICATION_JSON)
+                )
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(content().string(containsString("\"postId\":1")));
+    }
+    @Test
+    @Rollback
+    @Transactional
+    public void getTest() throws Exception{
+        this.mockMvc.perform(post("/api/people/get")
+                        .content("1")
+                        .contentType(MediaType.APPLICATION_JSON)
+                )
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(content().string(containsString("\"postId\":1")));
+    }
+}
