543dad24378be0c0ba3dcdf102426bc94f430dd2
bna <skeletonpro1@mail.ru>
2023-03-16 19:47:29 +0500

    Обновление swagger (+авторизация) и getlist для controlobject

--- src/main/java/biz/spring/core/config/SwaggerConfig.java
+++ src/main/java/biz/spring/core/config/SwaggerConfig.java
@@ -1,8 +1,13 @@
 package biz.spring.core.config;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeIn;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
+import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Contact;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.info.License;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
 import io.swagger.v3.oas.models.servers.Server;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
@@ -14,6 +19,13 @@ import java.util.List;
  *  localhost:8080/swagger-ui.html
  */
 @Configuration
+@SecurityScheme(
+        name = "Bearer Authentication",
+        type = SecuritySchemeType.HTTP,
+        bearerFormat = "Authorization",
+        scheme = "Bearer",
+        in= SecuritySchemeIn.HEADER
+)
 public class SwaggerConfig {
     @Bean
     public OpenAPI customOpenApi(@Value("${application-description}")String appDescription,
@@ -25,7 +37,19 @@ public class SwaggerConfig {
                                 .url("http://springdoc.org"))
                         .contact(new Contact().name("bna")
                                 .email("n_bryklia@bk.ru")))
+                .addSecurityItem(new SecurityRequirement().addList("Bearer Authentication"))
+                .components(
+                        new Components()
+                                .addSecuritySchemes("Bearer Authentication",
+                                        new io.swagger.v3.oas.models.security.SecurityScheme()
+                                                .name("Bearer Authentication")
+                                                .type(io.swagger.v3.oas.models.security.SecurityScheme.Type.HTTP)
+                                                .scheme("bearer")
+                                                .bearerFormat("JWT")
+                                )
+                )
                 .servers(List.of(new Server().url("http://localhost:8080")
                         .description("Dev service")));
+
     }
 }
--- src/main/java/biz/spring/core/service/ControlObjectService.java
+++ src/main/java/biz/spring/core/service/ControlObjectService.java
@@ -3,6 +3,7 @@ package biz.spring.core.service;
 import biz.spring.core.model.AccessRole;
 import biz.spring.core.model.ControlObject;
 import biz.spring.core.repository.ControlObjectRepository;
+import biz.spring.core.utils.GridDataOption;
 import biz.spring.core.utils.Query;
 import biz.spring.core.view.AccessRoleView;
 import biz.spring.core.view.ControlObjectView;
@@ -23,16 +24,22 @@ public class ControlObjectService extends BaseService<ControlObject> {
 
     private final String mainSql = "" +
             "SELECT * " +
-            "FROM controlobject";
+            "FROM   controlobject co " +
+            "       LEFT JOIN controlobjectrole cor ON co.controlobject_id = cor.controlobject_id " +
+            "WHERE  1=1 " +
+            "  AND  cor.accessrole_id = :accessRoleId";
 
     private final String mainSqlForOne = "" +
             "SELECT * " +
             "FROM controlobject " +
             "WHERE controlobject_id = :id";
 
-    public List<ControlObjectView> getAll(){
+    public List<ControlObjectView> getAll(GridDataOption gridDataOption){
         return new Query<ControlObjectView>(mainSql)
                 .forClass(ControlObjectView.class)
+                .setLimit(gridDataOption.buildPageRequest())
+                .setOrderBy(gridDataOption.getOrderBy())
+                .setParams(gridDataOption.buildParams())
                 .execute();
     }
 
