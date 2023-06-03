5eae4fd97b9685243aaab39fbe5ccac04e78e0d7
bna <skeletonpro1@mail.ru>
2023-06-03 20:18:01 +0500

    Перенёс файлы в core

--- src/main/resources/script/dnk/appendix/mainSqlForOne.sql
+++
@@ -1,9 +0,0 @@
-SELECT m0.*,
-       BL.block_id,
-       MA.material_id,
-       DR.documentreal_id
-FROM   appendix m0
-	INNER JOIN documentreal DR ON m0.appendix_id = DR.documentreal_id
-	LEFT JOIN material MA ON m0.appendix_id = MA.material_id
-	LEFT JOIN block BL ON MA.block_id = BL.block_id
-WHERE  m0.appendix_id = :id
