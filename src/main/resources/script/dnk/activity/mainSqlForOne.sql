c6912c9ccd5889d51560e2d0ef15022ef66d2444
bna <skeletonpro1@mail.ru>
2023-06-05 00:01:40 +0500

    bug fix

--- src/main/resources/script/dnk/activity/mainSqlForOne.sql
+++ src/main/resources/script/dnk/activity/mainSqlForOne.sql
@@ -7,9 +7,9 @@ SELECT m0.*,
        cc.capclass_name,
        c.company_name
 FROM   activity m0
-       INNER JOIN address a on m0.address_id = a.address_id
-       INNER JOIN town t on a.town_id = t.town_id
-       INNER JOIN street s on a.street_id = s.street_id
+       LEFT JOIN address a on m0.address_id = a.address_id
+       LEFT JOIN town t on a.town_id = t.town_id
+       LEFT JOIN street s on a.street_id = s.street_id
        INNER JOIN capclass cc on m0.capclass_id = cc.capclass_id
        LEFT JOIN company c on a.address_id = c.address_id
 WHERE  m0.activity_id = :id
