e7e9e007413736718da9f24c477b23a27c176cb8
bna <skeletonpro1@mail.ru>
2023-06-04 21:40:26 +0500

    test fix

--- src/main/resources/sql/dnk/200500-studycase.sql
+++ src/main/resources/sql/dnk/200500-studycase.sql
@@ -7,3 +7,4 @@ CREATE TABLE studycase (
     studycase_visible INTEGER NOT NULL,
     CONSTRAINT studycase_pk PRIMARY KEY (studycase_id)
 );
+CREATE SEQUENCE studycase_id_gen INCREMENT BY 1 MINVALUE 1 MAXVALUE 2147483647 START 1 CACHE 1 NO CYCLE;
